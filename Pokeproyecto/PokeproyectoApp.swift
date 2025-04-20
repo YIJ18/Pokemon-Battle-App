d7e1d7b253debcbd3aa3ba974e24835ecf845a28
YIJ18 <jassoiris96@gmail.com>
2025-04-20 13:17:29 -0600

    Initial Commit

---
+++ Pokeproyecto/PokeproyectoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeproyectoApp.swift
+//  Pokeproyecto
+//
+//  Created by Iris Jasso on 20/04/25.
+//
+
+import SwiftUI
+
+@main
+struct PokeproyectoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
